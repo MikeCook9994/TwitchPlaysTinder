c06e3ff95a45586839528c9f1f555647e8b73ddd
Michael Cook <mcook4728@gmail.com>
2018-03-11 12:55:33 -0700

    using sass now. Plugged in electron.

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron')
+
+let window;
+
+function createWindow() {
+    // Create the browser windows
+    window = new BrowserWindow({
+        backgroundColor: '#ffffff',
+        icon: `file://${__dirname}/dist/assests/logo.png`
+    });
+
+    window.loadURL(`file://${__dirname}/dist/index.html`);
+
+    // Uncoment below to open the DevTools.
+    // win.webContents.openDevTools();
+
+    // Event when the window is closed.
+    window.on('closed', function() {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed
+app.on('Window-all-closed', function() {
+    // On macOS specific close process
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function() {
+    //macOS specific close process
+    if(win === null) {
+        createWindow();
+    }
+});
