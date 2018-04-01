be8180bd214c1f25110107d2ee17b4a32edd18c8
Michael Cook <mcook4728@gmail.com>
2018-03-31 22:33:26 -0700

    add service for tinder auth.

--- src/app/modules/root/app.component.ts
+++ src/app/modules/root/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from '../../services/auth/auth.service';
+import { TinderAuthInfo } from '../../models/tinderAuthInfo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Twitch Plays Tinder';
+export class AppComponent implements OnInit {
+
+  public authInfo: TinderAuthInfo;
+
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authService.GetAuthInfo('', '').then((authInfo: TinderAuthInfo) => {
+      this.authInfo = authInfo;
+    });
+  }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { launch as OpenBrowser, Browser, Page } from 'puppeteer';
+
+import { TinderAuthInfo } from '../../models/tinderAuthInfo';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
+  /**
+   * Given the e-mail address and password of a Facebook account, retrieves the Tinder app auth token and the user id
+   * of the associated Facebook account for authenticating with Tinder via their API.
+   *
+   * @param {string} email the e-mail address of the facebook account to get a tinder auth token for
+   * @param {string} password the password of the facebook account to get a tinder auth token for
+   * @returns {Promise<TinderAuthInfo>} contains the Tinder auth token and the user id of the associated Facebook user
+   * account
+   * @memberof AuthService
+   */
+  public GetAuthInfo(email: string, password: string): Promise<TinderAuthInfo> {
+    return OpenBrowser()
+      .then((browser: Browser) => browser.newPage())
+      .then((page: Page) => null);
+  }
 }
