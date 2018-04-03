0674bad67fa9206b6349fd69f8c56a62cc1397d8
Michael Cook <mcook4728@gmail.com>
2018-04-03 00:45:42 -0700

    some hacky electron app that spawns a child process.

--- main.ts
+++ main.ts
@@ -1,46 +1,44 @@
 import { app, BrowserWindow } from 'electron';
-import { spawn } from 'child_process';
+import * as child_process from 'child_process';
 
 (() => {
-    let window: BrowserWindow = createWindow();
-    registerElectronEventListeners(window);
-    const server = spawn('node', ['./server/main.js']);
-})();
-
-function createWindow(): BrowserWindow {
-    // Create the browser windows
-    let window: BrowserWindow = new BrowserWindow({
-        backgroundColor: '#ffffff',
-        icon: `file://${__dirname}/dist/assests/logo.png`
-    });
-
-    window.loadURL(`file://${__dirname}/ux/dist/index.html`);
-
-    // Uncoment below to open the DevTools.
-    // win.webContents.openDevTools();
-
-    // Event when the window is closed.
-    window.on('closed', function() {
-        window = null;
-    });
-
-    return window;
-}
-function registerElectronEventListeners(window: BrowserWindow): void {
-    app.on('ready', createWindow);
-
-    // Quit when all windows are closed
-    app.on('window-all-closed', function() {
-        // On macOS specific close process
-        if(process.platform !== 'darwin') {
-            app.quit();
-        }
-    });
+    function createWindow() {
+        window = new BrowserWindow({
+            backgroundColor: '#ffffff',
+        });
     
-    app.on('activate', function() {
-        //macOS specific close process
-        if(window === null) {
-            createWindow();
-        }
+        window.loadURL(`file://${__dirname}/ux/index.html`);
+    
+        window.on('closed', function() {
+            window = null;
+        });
+    }
+    
+    function registerElectronEventListeners(): void {
+        app.on('ready', createWindow);
+    
+        // Quit when all windows are closed
+        app.on('window-all-closed', function() {
+            // On macOS specific close process
+            if(process.platform !== 'darwin') {
+                app.quit();
+            }
+        });
+        
+        app.on('activate', function() {
+            //macOS specific close process
+            if(window === null) {
+                createWindow();
+            }
+        });
+    }
+    
+    let window: BrowserWindow;
+    
+    registerElectronEventListeners();
+
+    const server = child_process.spawn('node', ['./dist/server/src/main.js']);
+    server.stdout.on('data', (data) => { 
+        console.log(`stdout: ${data}`);
     });
-}
+})();
